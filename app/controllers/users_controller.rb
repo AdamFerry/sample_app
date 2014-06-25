5e62a17ffbeb096cd6f57b150eb58504726acf0a
Adam J. Ferry <adam.james.ferry@gmail.com>
2014-06-24 23:18:07 -0400

    Add user microposts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,8 @@ class UsersController < ApplicationController
   before_action :signed_in_dont_create, only: [:new, :create]
 
   def show
-	@user = User.find(params[:id])
+    @user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(page: params[:page])
   end
 
   def index
@@ -54,13 +55,6 @@ def create
     
     
     # Before filters
-    
-    def signed_in_user
-      unless signed_in?
-        store_location
-        redirect_to signin_url, notice: "Please sign in."
-      end
-    end
     
     def correct_user
       @user = User.find(params[:id])
